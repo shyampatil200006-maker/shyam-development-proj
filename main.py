527a2a7da5a2f66332ee9a61eb9b2fa9c2f524f2
shyampatil200006-maker <shyampatil200006@gmail.com>
2025-09-08 17:46:00 +0000

    added bootstrap styling and a python list data

--- main.py
+++ main.py
@@ -8,17 +8,28 @@ from flask import Flask, render_template
 
 flaskapp = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': '1200000'
+}, {
+    'id': 2,
+    'title': 'Project Manager',
+    'location': 'Navi Mumbai, India',
+    'salary': '1800000'
+}, {
+    'id': 3,
+    'title': "SQL Developer",
+    'location': 'Navi Mumbai, India',
+    'salary': '1500000'
+}]
+
 
 @flaskapp.route('/')
 def home():
-  """Home page route."""
-  return render_template('home.html')
-
-
-@flaskapp.route('/about')
-def about():
-  """About page route."""
-  return render_template('home.html')
+  """Home page route. with multiple parameters for job list , banking type"""
+  return render_template('home.html', availjobs=JOBS, bnktype="Private")
 
 
 if __name__ == "__main__":
